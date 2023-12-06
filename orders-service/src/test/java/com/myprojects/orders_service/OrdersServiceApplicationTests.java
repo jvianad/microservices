4c21af3f36fe50523f18b7c7a9fa5dd62ef339c2
jvianad <jeanvianademares@gmail.com>
2023-12-06 01:22:17 -0500

    Commit 1:Se inicia proyecto con microservicios, definimos estructura

---
+++ orders-service/src/test/java/com/myprojects/orders_service/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
